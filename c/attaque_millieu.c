144967736780be76481898ed3ac03e3d30879296
Joly Thomas <72306726+tThomasJolyY@users.noreply.github.com>
2024-10-26 14:32:12 +0200

    Update attaque_millieu.c

--- c/attaque_millieu.c
+++ c/attaque_millieu.c
@@ -8,7 +8,7 @@
 #include "tri.h"
 
 /*
-commande pour compiler : gcc -o attaque attaque_millieu.c -lm
+commande pour compiler : make
 
 boite S :
 x    | 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
